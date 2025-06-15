0e752230ff7e4c89a88eaf80dc45617c4e11c328
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-06-14 23:03:10 -0400

    Mode Dev and Production

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { StatusCodes } from "http-status-codes";
+import { env } from "~/config/environment";
 
 export const errorHandlingMiddleware = (err, req, res, next) => {
   if (!err.statusCode) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
@@ -9,5 +10,8 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
     message: err.message || StatusCodes[err.statusCode],
     stack: err.stack,
   };
+
+  if (env.BUILD_MODE != "dev") delete responseError.stack;
+
   res.status(responseError.statusCode).json(responseError);
 };
