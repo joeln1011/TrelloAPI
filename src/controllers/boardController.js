4020e5a472943b6dae1fe207dcc7a07c5e453aff
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-06-14 23:43:52 -0400

    Service Layer

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,9 +1,14 @@
 import { StatusCodes } from "http-status-codes";
 import ApiError from "~/utils/ApiError";
+import { boardService } from "~/services/boardService";
 
 const createNew = async (req, res, next) => {
   try {
-    throw new ApiError(StatusCodes.BAD_GATEWAY, "Error Testing....");
+    //
+    const createdBoard = await boardService.createNew(req.body);
+
+    res.status(StatusCodes.CREATED).json({ message: createdBoard });
+    // throw new ApiError(StatusCodes.BAD_GATEWAY, "Error Testing....");
   } catch (error) {
     next(error);
   }
