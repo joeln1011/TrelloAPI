0d0a3bfb000d09d743af49004fe69a0132b37a06
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-06-19 23:31:54 -0400

    Add API For New Column And Card

---
+++ src/services/columnService.js
@@ -0,0 +1,20 @@
+import { columnModel } from "~/models/boardModel";
+
+const createNew = async (reqBody) => {
+  try {
+    const newColumn = { ...reqBody };
+    const createdColumn = await columnModel.createNew(newColumn);
+    const getNewColumn = await columnModel.findOneById(
+      createdColumn.insertedId
+    );
+
+    return getNewColumn;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const columnService = {
+  createNew,
+  getDetails,
+};
