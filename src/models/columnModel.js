64b3d874418d2d032efb2f9fbda5205e9cc0ebb3
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-06-21 00:13:40 -0400

    Add API for Column and Card

--- src/models/columnModel.js
+++ src/models/columnModel.js
@@ -1,4 +1,6 @@
 import Joi from "joi";
+import { ObjectId } from "mongodb";
+import { GET_DB } from "~/config/mongodb";
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from "~/utils/validators";
 
 // Define Collection (name & schema)
@@ -19,7 +21,59 @@ const COLUMN_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false),
 });
 
+const createNew = async (data) => {
+  try {
+    const validData = await validateBeforeCreate(data);
+    const NewColumnToAdd = {
+      ...validData,
+      boardId: new ObjectId(validData.boardId),
+    };
+    const createdColumn = await GET_DB()
+      .collection(COLUMN_COLLECTION_NAME)
+      .insertOne(NewColumnToAdd);
+    return createdColumn;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const validateBeforeCreate = async (data) => {
+  return await COLUMN_COLLECTION_SCHEMA.validateAsync(data, {
+    abortEarly: false,
+  });
+};
+const findOneById = async (id) => {
+  try {
+    const result = await GET_DB()
+      .collection(COLUMN_COLLECTION_NAME)
+      .findOne({
+        _id: new ObjectId(id),
+      });
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+// Push CardId to columnOrderIds at the end of the column
+const pushCardOrderIds = async (card) => {
+  try {
+    const result = await GET_DB()
+      .collection(COLUMN_COLLECTION_NAME)
+      .findOneAndUpdate(
+        { _id: new ObjectId(card.columnId) },
+        { $push: { cardOrderIds: new ObjectId(card._id) } },
+        { returnDocument: "after" }
+      );
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 export const columnModel = {
   COLUMN_COLLECTION_NAME,
   COLUMN_COLLECTION_SCHEMA,
+  createNew,
+  findOneById,
+  pushCardOrderIds,
 };
