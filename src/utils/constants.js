14f9f6abf1147acbbfbaf44941608b842790a900
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-06-24 19:56:01 -0400

    Update Whitelist Domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 //Domains allow to access to server
-export const WHITELIST_DOMAINS = [];
+export const WHITELIST_DOMAINS = ["https://trello-web-pink-alpha.vercel.app"];
 
 export const BOARD_TYPES = { PUBLIC: "public", PRIVATE: "private" };
