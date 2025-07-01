8c35903bd134f7140e3e667a4d8935a34f527e54
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-06-30 20:00:56 -0400

    Eaas MailerSend

---
+++ src/providers/MailerSendProvider.js
@@ -0,0 +1,34 @@
+import { env } from '~/config/environment';
+import { MailerSend, EmailParams, Sender, Recipient } from 'mailersend';
+
+const MAILER_SEND_API_KEY = env.MAILER_SEND_API_KEY;
+const ADMIN_SENDER_EMAIL = env.ADMIN_SENDER_EMAIL;
+const ADMIN_SENDER_NAME = env.ADMIN_SENDER_NAME;
+
+const mailerSendInstance = new MailerSend({
+  apiKey: MAILER_SEND_API_KEY,
+});
+
+const sendFrom = new Sender(ADMIN_SENDER_EMAIL, ADMIN_SENDER_NAME);
+const sendEmail = async ({ to, toName, subject, html }) => {
+  try {
+    // Create a new Recipient object
+    const recipient = [new Recipient(to, toName)];
+
+    // Create a new EmailParams object
+    const emailParams = new EmailParams()
+      .setFrom(sendFrom)
+      .setTo(recipient)
+      .setReplyTo(sendFrom)
+      .setSubject(subject)
+      .setHtml(html);
+
+    const data = await mailerSendInstance.email.send(emailParams);
+    return data;
+  } catch (error) {
+    console.error('MailerSend sendEmail error:', error);
+    throw error;
+  }
+};
+
+export const MailerSendProvider = { sendEmail };
--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,17 +1,17 @@
 export const OBJECT_ID_RULE = /^[0-9a-fA-F]{24}$/;
 export const OBJECT_ID_RULE_MESSAGE =
-  "Your string fails to match the Object Id pattern!";
+  'Your string fails to match the Object Id pattern!';
 
 // Regular expression and error messages for validation
-export const FIELD_REQUIRED_MESSAGE = "This field is required";
+export const FIELD_REQUIRED_MESSAGE = 'This field is required';
 export const EMAIL_RULE = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const EMAIL_RULE_MESSAGE = {
-  "string.pattern.base": "Invalid email format.",
+  'string.pattern.base': 'Invalid email format.',
 };
 export const PASSWORD_RULE = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,256}$/;
 export const PASSWORD_RULE_MESSAGE = {
-  "string.pattern.base":
-    "Password must be at least 8 characters, include letters and numbers.",
+  'string.pattern.base':
+    'Password must be at least 8 characters, include letters and numbers.',
 };
 
-export const PASSWORD_CONFIRMATION_MESSAGE = "Passwords do not match";
+export const PASSWORD_CONFIRMATION_MESSAGE = 'Passwords do not match';
